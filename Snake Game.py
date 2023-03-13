0b2ca60708ce3e48c4726c2f9805be1dc6bb5348
Matthew Robertson <123426720+mr-obertson@users.noreply.github.com>
2023-03-13 18:52:14 -0400

    Fixed snake, Take the L, Get Good

--- Snake Game.py
+++ Snake Game.py
@@ -18,7 +18,7 @@ pygame.display.set_caption('Paul\'s Snake Game')
 
 clock = pygame.time.Clock()
 
-snake_block = 10
+snake_block = 25
 snake_speed = 15
 
 font_style = pygame.font.SysFont("comicsansms", 25)
@@ -30,7 +30,8 @@ def Your_score(score):
     dis.blit(value, [0, 0])
 def our_snake(snake_block, snake_list):
     for x in snake_list:
-        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+        pygame.draw.rect(dis, black, [round(x[0] / snake_block) * snake_block, round(x[1] / snake_block) * snake_block, snake_block, snake_block])
+
 
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
@@ -44,8 +45,9 @@ def gameLoop():
     y1_change = 0
     snake_List = []
     Length_of_snake = 1
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+    foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+
     while not game_over:
 
         while game_close == True:
@@ -94,9 +96,10 @@ def gameLoop():
         our_snake(snake_block, snake_List)
         Your_score(Length_of_snake - 1)
         pygame.display.update()
-        if x1 == foodx and y1 == foody:
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+        if round(abs(x1 - foodx)) < snake_block and round(abs(y1 - foody)) < snake_block:
+            foodx = round(random.randrange(0, dis_width - snake_block) / snake_block) * snake_block
+            foody = round(random.randrange(0, dis_height - snake_block) / snake_block) * snake_block
+
             Length_of_snake += 1
         clock.tick(snake_speed)
     pygame.quit()
