3a137b2c69174322d16849628727c4df25ca9437
SKel1817 <123426684+SKel1817@users.noreply.github.com>
2023-04-09 17:12:53 -0400

    god please bless this file upload
    don't make me wait 3 days

--- test_main.py
+++ test_main.py
@@ -1,4 +1,4 @@
-from main import set_page_return_current_page
+from tests import set_page_return_current_page
 
 def test_set_page_return_current_page():
     assert set_page_return_current_page(3) == 3
---
+++ tests.py
@@ -0,0 +1,3 @@
+def set_page_return_current_page(pagenum):
+    # current_games = current_page(pagenum)
+    return pagenum
