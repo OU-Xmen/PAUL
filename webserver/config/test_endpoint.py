cbea06ff3b5286acefd4fdad0a5309c74420a646
Eli Sepulveda <elikartwii@gmail.com>
2023-04-16 01:08:30 -0400

    Add files via upload

--- webserver/config/test_endpoint.py
+++ webserver/config/test_endpoint.py
@@ -1,17 +1,17 @@
 import requests
 
 # [ Options ]
-domain = "https://web.physcorp.com/paul/endpoint.php"
+domain = "https://paulis.online/endpoint"
 
 # List of games
 valid_games = ['Puzzle', 'Asteroids', 'Tetris', 'Pong', 'Hangman', 'Mad Libs', 'Checkers', 'Chess', 'Guess the Number', 'Snake', 'Tic Tac Toe', 'Connect 4', 'Mancala', 'RPS', 'PAULatformer']
 
 # Make POST request with name, score, and game to endpoint to add score
-r = requests.post(domain, data = {'name': 'HELP I AM TRAPPED IN A COMPUTER PLEASE LET ME OUT', 'score': 9001, 'game': 'Puzzle'})
-# Print status code and response
-print(f"[Code {r.status_code}] {r.text}")
-
-# Make GET request to endpoint to retrieve scores
-# r = requests.get(domain)
+# r = requests.get(domain, params={'task':'put', 'name': 'HELP I AM TRAPPED IN A COMPUTER PLEASE LET ME OUT', 'score': 9001, 'game': 'Puzzle'})
 # Print status code and response
 # print(f"[Code {r.status_code}] {r.text}")
+
+# Make GET request to endpoint to retrieve scores
+r = requests.get(domain)
+# Print status code and response
+print(f"[Code {r.status_code}] {r.text}")
